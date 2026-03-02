aca886574fdac4f340a4ee85344eae18ac288715
dilanthanayanajith <dilanthanayanajith@gmail.com>
2026-03-03 01:30:09 +0530

    add dbconnection utility class for mysqL

---
+++ src/main/java/util/DBConnection.java
@@ -0,0 +1,31 @@
+package util;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class DBConnection {
+
+    private static final String URL = "jdbc:mysql://localhost:3306/room_reservation_db";
+    private static final String USER = "root";
+    private static final String PASSWORD = "xxxxx@75";
+
+    public static Connection getConnection() {
+        try {
+            // Load MySQL Driver
+            Class.forName("com.mysql.cj.jdbc.Driver");
+
+            // Create Connection
+            return DriverManager.getConnection(URL, USER, PASSWORD);
+
+        } catch (ClassNotFoundException e) {
+            System.out.println("MySQL Driver not found!");
+            e.printStackTrace();
+        } catch (SQLException e) {
+            System.out.println("Database connection failed!");
+            e.printStackTrace();
+        }
+
+        return null;
+    }
+}
