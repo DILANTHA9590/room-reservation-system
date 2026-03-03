e5977f4bd153528ea39974e8f97ebafb9c79f0f9
dilanthanayanajith <dilanthanayanajith@gmail.com>
2026-03-03 10:10:32 +0530

    implement login servlet

---
+++ src/main/java/controller/LoginServlet.java
@@ -0,0 +1,40 @@
+package controller;
+
+import dao.UserDAO;
+import model.User;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.*;
+import java.io.IOException;
+
+@WebServlet("/login")
+public class LoginServlet extends HttpServlet {
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+
+        String email = request.getParameter("email");
+        String password = request.getParameter("password");
+
+        UserDAO dao = new UserDAO();
+        User user = dao.login(email, password);
+
+        if (user == null) {
+            request.setAttribute("error", "Invalid Email or Password");
+            request.getRequestDispatcher("view/login.jsp").forward(request, response);
+            return;
+        }
+
+        HttpSession session = request.getSession();
+        session.setAttribute("loggedUser", user);
+
+        // redirect by role
+        if ("ADMIN".equalsIgnoreCase(user.getRole())) {
+            response.sendRedirect(request.getContextPath() + "/admin/dashboard.jsp");
+        } else {
+            response.sendRedirect(request.getContextPath() + "/staff/dashboard.jsp");
+        }
+    }
+}
